07ba2c9a24e6a1a2d1597ece5dbca84019e88189
mhdianrush <mhdianrush@gmail.com>
2023-07-11 11:16:22 +0700

    Added for Index, Find, Create, Update, and Delete Function

---
+++ controllers/product.controller.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func Index(c *gin.Context) {
+
+}
+
+func Find(c *gin.Context) {
+
+}
+
+func Create(c *gin.Context) {
+
+}
+
+func Update(c *gin.Context) {
+
+}
+
+func Delete(c *gin.Context) {
+
+}
