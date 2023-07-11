f99964615c57be07e4cc5e1b6b666247c9f97d2d
mhdianrush <mhdianrush@gmail.com>
2023-07-11 11:15:23 +0700

    Developed and Added for http method, endpoint on the routes

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mhdianrush/go-gin-product-api/config"
+	"github.com/mhdianrush/go-gin-product-api/controllers"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	config.ConnectDB()
 	r := gin.Default()
+
+	r.GET("/api/products", controllers.Index)
+	r.GET("/api/product/:id", controllers.Find)
+	r.POST("/api/product", controllers.Create)
+	r.PUT("/api/product/:id", controllers.Update)
+	r.DELETE("/api/product", controllers.Delete)
+
+	r.Run()
 
 	logger := logrus.New()
 	file, err := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
